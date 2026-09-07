a77ee676dafd2a2605243f675339e26c61831661
hrishikesh-thakare <hrishikeshthakare0809@gmail.com>
2026-09-07 20:28:49 +0530

    Fix ts error

--- frontend/src/components/editor/MentionList.tsx
+++ frontend/src/components/editor/MentionList.tsx
@@ -67,7 +67,6 @@ export const MentionList = forwardRef<MentionListHandle, MentionListProps>(funct
 
   return (
     <Popover open={true}>
-      {/* @ts-expect-error -- Radix UI virtualRef types don't strictly match here in older versions, but it works */}
       <PopoverAnchor virtualRef={virtualRef} />
       <PopoverContent
         onOpenAutoFocus={(e) => e.preventDefault()}
